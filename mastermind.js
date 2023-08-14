7c2fd868508aa2d1769d352ddc89c5c8c3aa224f
MAITUMELO RADIPODI <mradipodi@gmail.com>
2023-08-14 21:47:44 +0200

    mastermins complete

--- mastermind.js
+++ mastermind.js
@@ -1,75 +1,38 @@
-// https://jsdoc.app
-/**
- * @function checkGuess
- * Checks guess for "mastermind" game against solution
- *
- * @param {string} guess - the solution to the
- * @param {string} solution - the target for the guess
- *
- * @returns {string} - an string representing the number of correct numbers
- *                     in the correct position and the number of correct
- *                     numbers in the incorrect position for the guess
- *
- * @example
- * checkGuess('1532, '1234')
- * // returns '2-1'
- * // two numbers in the correct place (1 and 3)
- * // and one correct number in the incorrect place (2)
- *
- */
-function checkGuess(guess, solution) {
-  // TODO: complete this function
-  // first determine how many characters total the two strings have in common
-  // This may help:
-  // https://github.com/bonnie/udemy-ENZYME/blob/master/context-base/src/helpers/index.js
-  //
-  // then determine how many of those characters are in the right place
-  // hint: iterate through characters of guess and compare to character
-  // in the same position in solution
-  //
-  // finally, return a string in the format
-  // "count of correct characters in the right place"-"count of correct
-  // characters not in the right place"
-  // for example, "2-1"
-  //
-}
+const calculateHint = (secret, guess) => {
+  let correctPositions = 0;
+  let correctDigits = 0;
+  const digitCount = new Array(10).fill(0);
 
-// https://jsdoc.app
-/**
- * @function processInput
- * Checks guesses for "mastermind" game against solution
- *
- * @param {string} solution - the target for the guesses
- * @param {string[]} guesses - an array of strings representing guesses
- *
- * @returns {string[]} - an array of strings representing the number of
- *                       correct numbers in the correct position and the number
- *                       of correct numbers in the incorrect position for each
- *                       guess
- *
- * @example
- * // returns ['2-1', '0-1']
- * processInput('1234', ['1532', '8793'])
- *
- */
-function processInput(solution, guesses) {
-  return guesses.map((guess) => checkGuess(guess, solution));
-}
+  for (let i = 0; i < 4; i++) {
+      if (secret[i] === guess[i]) {
+          correctPositions++;
+      } else {
+          digitCount[parseInt(secret[i])] += 1;
+      }
+  }
 
-// ----------- main program ------- //
-// process arguments via destructuring
-//
-const [solution, guessCount, ...guesses] = process.argv.slice(2);
+  for (let i = 0; i < 4; i++) {
+      if (secret[i] !== guess[i] && digitCount[parseInt(guess[i])] > 0) {
+          correctDigits++;
+          digitCount[parseInt(guess[i])] -= 1;
+      }
+  }
 
-// (lightly) verify the input
-if (guesses.length !== Number(guessCount)) {
-  console.warn(
-    `The number of guesses provided (${guesses.length}) does not match the guess count (${guessCount}).`
-  );
-  console.warn("Exiting.");
-  process.exit(-1);
-}
+  return correctPositions + '-' + correctDigits;
+};
 
-// pass the input to the processor and print the output
-const output = processInput(solution, guesses);
-console.log(output.join(" "));
+const main = () => {
+  const args = process.argv.slice(2);
+  const secret = args[0];
+  const numGuesses = parseInt(args[1]);
+  const guesses = args.slice(2);
+
+  for (let i = 0; i < numGuesses; i++) {
+      const hint = calculateHint(secret, guesses[i]);
+      process.stdout.write(hint + ' ');
+  }
+
+  process.stdout.write('\n');
+};
+
+main();
